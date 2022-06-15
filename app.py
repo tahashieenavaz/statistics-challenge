85af91af732d930822edbce383ac0aa31979b34d
Taha Shieenavaz <tahashieenavaz@gmail.com>
2022-06-15 20:24:07 +0430

    Input title allowance addeD

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import csv
 from os import name, system
 
 def main():
+    allowedInputTitles = range(1397, 1401)
     yearSubjectCount = {
             1400: 4,
             1399: 5,
@@ -12,22 +13,29 @@ def main():
     inputCount = 4
     studentData = {}
     for i in range(1, inputCount + 1):
-        inputTitle = int(input("Enter input title: ")) # 1400, 1397
+        inputTitle = None
+
+        while True:
+            inputTitle = int(input("Enter input title: ")) 
+            if inputTitle in allowedInputTitles: break
+
         studentData[inputTitle] = []
         studentCount = int(input("Enter student count for the {}: ".format(inputTitle)))
+
         for p in range(1, studentCount + 1):
             scores = [] # [ "13:3", [20, 3] ]
-            studentNumber = int(input("Enter student number #{}: ".format(p)))
-            sex = input("Enter {}'s gender: ".format(studentNumber)) 
+            studentNumber = int(input("\tEnter student number #{}: ".format(p)))
+            sex = input("\tEnter {}'s gender: ".format(studentNumber)) 
 
             for z in range(1, yearSubjectCount[inputTitle] + 1):
-                factor = int(input("Enter subject #{}'s factor: ".format(z)))
-                score = float(input("Enter subject #{}'s score: ".format(z)))
+                factor = int(input("\t\tEnter subject #{}'s factor: ".format(z)))
+                score = float(input("\t\tEnter subject #{}'s score: ".format(z)))
                 scores.append(str(score) + ":" + str(factor)) 
 
             studentData[inputTitle].append([studentNumber, sex, scores])
         clearScreen()
-            
+        print(studentData)
+
 def saveArray(data):
     with open('database.csv', 'w+') as fh:
         csv.writer(fh, delimiter=",").writerows(data)
